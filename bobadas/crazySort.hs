07d273f514c992fa8cc0b5e143722120eba08da9
Nicolas Carlos Hernandez <nhernandez@ws11.labo1>
2014-11-17 16:17:27 -0300

    un gauss

--- bobadas/crazySort.hs
+++ bobadas/crazySort.hs
@@ -1,2 +1,8 @@
 crazySort [] = []
 crazySort (x : xs) = x : (crazySort (reverse xs) )
+
+casiGauss [] = []
+casiGauss (x:xs) = if xs == [] then [x] else (x + (head xs) ) : casiGauss(tail xs)
+
+gauss [] = 0
+gauss ls = (length (casiGauss (crazySort ls) )) * (head (casiGauss (crazySort ls) ))
