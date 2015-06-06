85b41e871f92aa4f68626743bcc01f0bb10fc550
Nicolas Hernandez <nicoh22@hotmail.com>
2015-06-06 20:50:01 -0300

    ejercicio c proba

--- proba/tp.r
+++ proba/tp.r
@@ -1,17 +1,63 @@
-ejuno<-function(n){
-cuenta<-c()
-x<-1:639
-t<-n
-while( n > 0){
-	z<-c()
-	count<-0
-	while( length(z) < 639){
-		z <- c(z,sample(x, 5, replace=FALSE))
-		z<-unique(z)
-		count <- count+1
+eja<-function(n){
+	cuenta<-c()
+	x<-1:639
+	t<-n
+	while( t > 0){
+		z<-c()
+		count<-0
+		while( length(z) < 639){
+			z <- c(z,sample(x, 5, replace=FALSE))
+			z<-unique(z)
+			count <- count+1
+		}
+		cuenta<-c(cuenta, count)
+		t<-t-1
 	}
-	cuenta<-c(cuenta, count)
-	n<-n-1
+	sum(cuenta)/n
 }
-sum(cuenta)/t
+
+ejb<-function(n){
+	cuenta<-c()
+	x<-1:639
+	t<-n
+	while( t > 0){
+		z<-c()
+		count<-0
+		while( length(z) < 639){
+			z <- c(z,sample(x, 5, replace=TRUE))
+			z<-unique(z)
+			count <- count+1
+		}
+		cuenta<-c(cuenta, count)
+		t<-t-1
+	}
+	sum(cuenta)/n
+}
+
+ejc<-function(n){
+	cuenta<-c()
+	x<-1:639
+	t<-n
+	vpeso<- rep.int(c(1/2000,(1-10/2000)/639),c(10,629))
+	while( t > 0){
+		z<-c()
+		count<-0
+		while( length(z) < 639){
+			z <- c(z,sample(x, 5, replace=TRUE, prob=vpeso))
+			z<-unique(z)
+			count <- count+1
+		}
+		cuenta<-c(cuenta, count)
+		t<-t-1
+	}
+	sum(cuenta)/n
+}
+
+
+ejd<-function(n){
+	cuenta<-c()
+	x<-1:639
+	t<-n
+	
+	
 }
