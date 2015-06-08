c432cf64e2f482e5a9b4667e61fff9bc12666903
Nicolas Carlos Hernandez <nhernandez@ws7.labo1>
2015-06-08 15:51:47 -0300

    terminado tp proba, mañana optimizo y comento piola

--- proba/tp.r
+++ proba/tp.r
@@ -55,11 +55,45 @@ ejc<-function(n){
 
 
 ejd<-function(n){
-	cuenta<-c()
+	prim<-0 #dividido por n sera la media del que termino primero
+	seg<-0	#idem con el que termino segundo
 	x<-1:639
 	t<-n
-	while( t > 0){
-		
+	while( t > 0){ #ciclo cantidad especificada de veces
+		cuentag<-0
+		cuentap<-0
+		pedro<-x
+		gaston<-x
+		while(length(pedro) != 0 && length(gaston) !=0 ){ #hasta que alguno llene album
+			sobreg<-sample(x, 5, replace=FALSE)
+			sobrep<-sample(x, 5, replace=FALSE)
+			repp<-sobrep[! sobrep %in% pedro] #me quedo con los repetidos de Pedro
+			repg<-sobrep[! sobreg %in% gaston] #y de Gaston
+			pedro<-pedro[! pedro %in% sobrep]
+			gaston<-gaston[! gaston %in% sobreg]
+			pedro<-pedro[! pedro %in% repg]#Gaston le da sus repes a Pedro
+			gaston<-gaston[! gaston %in% repp]# y viceversa
+			cuentag<-cuentag+1
+			cuentap<-cuentap+1#cuento los sobres que compre			
+		}
+		if(length(pedro) == 0){#pedro termino primero
+			prim<-prim+cuentap 
+			while(length(gaston)!=0){ #sigue Gaston por su cuenta
+				sobreg<-sample(x, 5, replace=FALSE)
+				gaston<-gaston[! gaston %in% sobreg]
+				cuentag<-cuentag+1
+			}
+			seg<-seg+cuentag
+		}else{#gaston termino primero
+			prim<-prim+cuentag
+			while(length(pedro)!=0){ #sigue Pedro por su cuenta
+				sobrep<-sample(x, 5, replace=FALSE)
+				pedro<-pedro[! pedro %in% sobrep]
+				cuentap<-cuentap+1
+			}
+			seg<-seg+cuentap
+		}
+		t<-t-1
 	}
-	
+	c(prim/n, seg/n) #devuelve la media del que termino primero y del segundo
 }
