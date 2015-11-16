9db02b5ab329e62600119d8f84f698ff163366d8
Nicolas Hernandez <nicoh22@hotmail.com>
2015-11-16 13:13:31 -0300

    ecm del tpr

---
+++ proba/tpecm.r
@@ -0,0 +1,32 @@
+estimMV<-function(n, theta){
+	Nrep<-1000
+	i<-1
+	estimadores<-rep(NA, Nrep)
+	while(i <= Nrep){
+		muestra<-runif(n, max = theta)
+		estimadores[i]<-max(muestra)
+		i<-i+1
+	}
+	estimadores<-estimadores - theta
+	estimadores<-estimadores^2
+	suma<-sum(estimadores)
+	ecm<-suma/Nrep
+	ecm
+}
+
+estimMO<-function(n, theta){
+	Nrep<-1000
+	i<-1
+	estimadores<-rep(NA, Nrep)
+	while(i <= Nrep){
+		muestra<-runif(n, max = theta)
+		estimadores[i]<-2 * mean(muestra)
+		i<-i+1
+	}
+	estimadores<-estimadores - theta
+	estimadores<-estimadores^2
+	suma<-sum(estimadores)
+	ecm<-suma/Nrep
+	ecm
+}
+
