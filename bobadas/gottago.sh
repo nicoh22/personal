36d38fc80cbae45fe69bd79e3d14c3fc65b09724
Nicolas Carlos Hernandez <nhernandez@ws6.labo1>
2015-11-24 16:37:26 -0300

    fug

--- bobadas/gottago.sh
+++ bobadas/gottago.sh
@@ -2,5 +2,5 @@
 
 deluged
 echo "deluged" | atq 22:30
-echo "killall deluged && sudo rtcwake -t `date -d "tomorrow 08:00:00" +%s` -m disk"| atq 11:00
+echo "killall deluged && sudo rtcwake -t `date -d "today 22:00:00" +%s` -m disk"| atq 11:00
 
