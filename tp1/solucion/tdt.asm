e8cf36c79e6beebc32330d70ce291d392c43e86a
Nicolas Carlos Hernandez <nhernandez@ws19.labo4>
2016-03-23 18:52:01 -0300

    tabla de traducciones, archivos base

---
+++ tp1/solucion/tdt.asm
@@ -0,0 +1,74 @@
+; FUNCIONES de C
+  extern malloc
+  extern free
+  extern strcpy
+  extern tdt_agregar
+  extern tdt_borrar
+  
+; FUNCIONES
+  global tdt_crear
+  global tdt_recrear
+  global tdt_cantidad
+  global tdt_agregarBloque
+  global tdt_agregarBloques
+  global tdt_borrarBloque
+  global tdt_borrarBloques
+  global tdt_traducir
+  global tdt_traducirBloque
+  global tdt_traducirBloques
+  global tdt_destruir
+
+; /** defines offsets y size **/
+  %define TDT_OFFSET_IDENTIFICACION   0
+  %define TDT_OFFSET_PRIMERA          8
+  %define TDT_OFFSET_CANTIDAD        16
+  %define TDT_SIZE                   20
+
+section .text
+
+; =====================================
+; tdt* tdt_crear(char* identificacion)
+tdt_crear:
+
+; =====================================
+; void tdt_recrear(tdt** tabla, char* identificacion)
+tdt_recrear:
+
+; =====================================
+; uint32_t tdt_cantidad(tdt* tabla)
+tdt_cantidad:
+
+; =====================================
+; void tdt_agregarBloque(tdt* tabla, bloque* b)
+tdt_agregarBloque:
+
+; =====================================
+; void tdt_agregarBloques(tdt* tabla, bloque** b)
+tdt_agregarBloques:
+        
+; =====================================
+; void tdt_borrarBloque(tdt* tabla, bloque* b)
+tdt_borrarBloque:
+	jmp tdt_borrar
+        
+; =====================================
+; void tdt_borrarBloques(tdt* tabla, bloque** b)
+tdt_borrarBloques:
+        
+; =====================================
+; void tdt_traducir(tdt* tabla, uint8_t* clave, uint8_t* valor)
+tdt_traducir:
+        
+; =====================================
+; void tdt_traducirBloque(tdt* tabla, bloque* b)
+tdt_traducirBloque:
+
+; =====================================
+; void tdt_traducirBloques(tdt* tabla, bloque** b)
+tdt_traducirBloques:
+        
+; =====================================
+; void tdt_destruir(tdt** tabla)
+tdt_destruir:
+
+
