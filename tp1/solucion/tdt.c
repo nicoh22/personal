f0672238907a539cfa99a359a6533b79da7171f2
Nicolas Carlos Hernandez <nhernandez@ws16.labo5>
2016-03-29 20:24:29 -0300

    tdt_agregar fixes

--- tp1/solucion/tdt.c
+++ tp1/solucion/tdt.c
@@ -5,18 +5,34 @@ void tdt_agregar(tdt* tabla, uint8_t* clave, uint8_t* valor) {
 	tdtN1 *primNivel = tabla->primera;
 	if(primNivel == NULL){
 		tabla->primera = malloc(256*8);
+		primNivel = tabla->primera;
+		int j = 0;
+		while(j<256){
+			primNivel->entradas[j] = NULL;
+		}
 	}
 
 	tdtN2 *segNivel = primNivel->entradas[clave[1]];
 	if(segNivel == NULL){
-		primNivel->entradas[clave[1]] = malloc(256*8);
+		segNivel = malloc(256*8);
+		primNivel->entradas[clave[1]] = segNivel;
+		int j = 0;
+		while(j<256){
+			segNivel->entradas[j] = NULL;
+		}
 	}
 	
 	tdtN3 *tercNivel = segNivel->entradas[clave[2]];
 	
 	if(tercNivel == NULL){
-		segNivel->entradas[clave[2]] = malloc(256*16);
+		tercNivel = malloc(256*16);
+		segNivel->entradas[clave[2]] = tercNivel; 
 	//TODO: poner los campos "valido" en NULL
+		int j = 0;
+		while(j <256){
+			tercNivel->entradas[j].valido = 0;
+			j++;
+		}
 	}
 	uint8_t *valorInterno = tercNivel->entradas[clave[3]].valor.val;
 	int i = 0;
