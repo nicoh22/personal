efc053d392ddda75ca26a5fedaf8fd0848cb4c77
Nicolas Hernandez <nicoh22@hotmail.com>
2017-09-28 09:10:54 -0300

    Learning ruby.

---
+++ ruby/shirt.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+require 'shellwords'
+
+BUILTINS = {
+	'cd' => lambda { |dir = '/home/nicolas'| Dir.chdir(dir) }, #hack
+	'exit' => lambda { |code = 0| exit(code.to_i) },
+	'exec' => lambda { |*command| exec *command }
+}
+
+
+loop do
+	$stdout.print '-> '
+	line = $stdin.gets.strip 
+	command, *arguments = Shellwords.shellsplit(line)
+
+	if BUILTINS[command]
+		BUILTINS[command].call(*arguments)
+	else
+		pid = fork {
+			exec line
+		}
+		Process.wait pid
+	end
+
+
+end
+
